2c04a6fd7c0371570e3c22d7ff1700a71ca6f8fc
Asim Ihsan <ssh@sshemail.ihsan.io>
2024-01-06 07:16:46 -0500

    dev-setup - fix cdktf

--- {{ cookiecutter.cdktf_directory_name }}/scripts/dev-setup.sh
+++ {{ cookiecutter.cdktf_directory_name }}/scripts/dev-setup.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+# Function to log messages with a timestamp
+log() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] $1"
+}
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 pushd "$ROOT_DIR" >/dev/null
@@ -9,7 +14,7 @@ trap "popd > /dev/null" EXIT
 
 install_pre_commit() {
     if command -v pre-commit &>/dev/null; then
-        echo "pre-commit is already installed."
+        log "pre-commit is already installed."
     else
         if [[ "$OSTYPE" == "darwin"* ]]; then
             brew install pre-commit
@@ -17,23 +22,45 @@ install_pre_commit() {
             sudo apt update
             sudo apt install -y pre-commit
         else
-            echo "Unsupported OS for pre-commit installation."
+            log "Unsupported OS for pre-commit installation."
             exit 1
         fi
     fi
     pre-commit install
 }
 
-install_pre_commit
+install_cdktf_cli() {
+    if command -v cdktf &>/dev/null; then
+        log "cdktf-cli is already installed."
+    else
+        brew install cdktf
+    fi
+}
 
-npm install --global cdktf-cli@latest
-npm install
-poetry install --with=dev --no-root
+install_poetry_dependencies() {
+    if ! poetry install --with=dev --no-root; then
+        log "poetry install failed."
+        exit 1
+    fi
+}
+
+install_pre_commit
+install_cdktf_cli
+install_poetry_dependencies
 
 # Find subdirs with pyproject.toml in root, iterate over them
 while IFS= read -r d; do
-    echo "Installing poetry dependencies for $d"
+    log "Installing poetry dependencies for $d"
     (cd "$d" && poetry install --with=dev --no-root)
 done < <(find . -name "pyproject.toml" -exec dirname {} \;)
 
-brew install awscurl
+# Check if awscurl is installed via Homebrew and install if not
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if brew list awscurl &>/dev/null; then
+        log "awscurl is already installed via Homebrew."
+    else
+        brew install awscurl
+    fi
+else
+    log "awscurl installation is only supported on macOS via Homebrew."
+fi
